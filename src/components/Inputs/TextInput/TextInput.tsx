c09193e4d589e0b0b85c8b32db1e2dcc317f58a7
jaken92 <jaaakeeen@gmail.com>
2025-01-30 13:55:24 +0100

    Validation for contact form

--- src/components/Inputs/TextInput/TextInput.tsx
+++ src/components/Inputs/TextInput/TextInput.tsx
@@ -8,16 +8,31 @@ interface TextInputProps extends FieldRenderProps<string, HTMLElement> {
 
 interface StyledTextInputProps {
   showError: boolean;
+  errorMsg?: string;
 }
 
 const StyledTextInput = styled.div<StyledTextInputProps>(
-  ({ showError }) => css`
+  ({ showError, errorMsg, theme }) => css`
     display: flex;
     flex-direction: column;
     align-items: baseline;
     margin-bottom: "5px";
     width: 100%;
     overflow-x: none;
+    position: relative;
+
+    &::after {
+      content: ${errorMsg && showError ? `"${errorMsg}"` : ""};
+      color: red;
+      bottom: 0px;
+      right: 0px;
+      padding-inline: 5px;
+      height: 15px;
+      position: absolute;
+      background-color: transparent;
+      border-radius: 3px;
+      font-size: 10px;
+    }
 
     & > label {
       /* display: block; */
@@ -27,11 +42,17 @@ const StyledTextInput = styled.div<StyledTextInputProps>(
     & > input,
     textarea {
       padding: 8px;
-      border: ${showError ? "2px solid red" : "2px solid #ccc"};
+      border: ${showError ? "1px solid red" : "1px solid #ccc"};
       border-radius: 4px;
       font-size: 14px;
       width: 100%;
       resize: vertical;
+    }
+
+    & > input:focus,
+    textarea:focus {
+      /* outline: 1px solid ${theme.accent}; */
+      outline: ${showError ? "1px solid red" : `1px solid ${theme.accent}`};
     }
   `
 );
@@ -45,21 +66,25 @@ export const TextInput: React.FC<TextInputProps> = ({
 }) => {
   const showError = meta.touched && meta.error;
   const isTextArea = input.type === "textarea";
+  const errorMsg = meta.error;
+
+  console.log({ meta });
 
   return (
-    <StyledTextInput showError={showError}>
+    <StyledTextInput showError={showError} errorMsg={errorMsg}>
       {label && <label htmlFor={input.name}>{label}</label>}
+
       {isTextArea ? (
         <textarea {...input} {...props} />
       ) : (
         <input {...input} {...props} type={type} />
       )}
 
-      {showError && (
+      {/* {errorMsg && meta.active && (
         <div style={{ color: "red", fontSize: "12px", marginTop: "5px" }}>
           {meta.error}
         </div>
-      )}
+      )} */}
     </StyledTextInput>
   );
 };
--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -4,6 +4,41 @@ import styled, { css } from "styled-components";
 import { Field, Form } from "react-final-form";
 import { TextInput } from "../../components/Inputs/TextInput";
 import { usePostMail } from "../../hooks/mutations";
+import { useTheme } from "styled-components";
+
+//validators
+
+const required = (value: any) => (value ? undefined : "Required");
+
+const noNumbers = (value: any) =>
+  !/\d/.test(value) ? undefined : "Numbers not allowed in field";
+
+const noSpecialChars = (value: any) => {
+  const specCharRegex = /[^a-zA-Z0-9\s]/;
+
+  return specCharRegex.test(value)
+    ? "Special chararcters not allowed in field"
+    : undefined;
+};
+
+const isValidEmail = (value: any) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(value) ? undefined : "Enter a valid email address";
+};
+
+const noHtmlTags = (value: any) => {
+  const htmlRegex = /<(script|iframe)[^>]*>|<[^>]*>|<|\s*>|<\/\s*>/;
+
+  return htmlRegex.test(value) ? "Html characters not allowed." : undefined;
+};
+
+const composeValidators =
+  (...validators: any[]) =>
+  (value: any) =>
+    validators.reduce(
+      (error, validator) => error || validator(value),
+      undefined
+    );
 
 const StyledContactForm = styled.section(
   ({ theme }) => css`
@@ -55,6 +90,12 @@ const StyledContactForm = styled.section(
             flex-shrink: 0;
             margin: 0 0.5rem 0 0;
 
+            & > .spinner {
+              height: 15px;
+              animation: spin 1s linear infinite;
+              margin-inline: 8px;
+            }
+
             &:hover {
               background-color: ${theme.accent};
               color: ${theme.primary};
@@ -71,6 +112,15 @@ const StyledContactForm = styled.section(
         }
       }
     }
+
+    @keyframes spin {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
   `
 );
 
@@ -84,6 +134,10 @@ interface ContactFormData {
 
 export const Contact = () => {
   const mutation = usePostMail();
+
+  const theme = useTheme();
+
+  console.log("Current theme: ", theme.accent);
 
   const onSubmit = async (values: ContactFormData) => {
     console.log(values);
@@ -126,7 +180,13 @@ export const Contact = () => {
                       label="First Name"
                       placeholder="First Name"
                       type="name"
+                      validate={composeValidators(
+                        required,
+                        noNumbers,
+                        noSpecialChars
+                      )}
                     />
+                    <div></div>
                   </div>
                   <div>
                     <Field
@@ -135,6 +195,11 @@ export const Contact = () => {
                       label="Last Name"
                       placeholder="Last Name"
                       type="name"
+                      validate={composeValidators(
+                        required,
+                        noNumbers,
+                        noSpecialChars
+                      )}
                     />
                   </div>
                   <div>
@@ -144,6 +209,7 @@ export const Contact = () => {
                       label="Email Address"
                       placeholder="Enter your email"
                       type="email"
+                      validate={composeValidators(required, isValidEmail)}
                     />
                   </div>
                   <div>
@@ -153,6 +219,7 @@ export const Contact = () => {
                       label="Subject"
                       placeholder="Subject"
                       type="text"
+                      validate={composeValidators(required, noSpecialChars)}
                     />
                   </div>
                   <div>
@@ -162,6 +229,7 @@ export const Contact = () => {
                       label="Message"
                       placeholder="Write your message..."
                       type="textarea"
+                      validate={composeValidators(required, noHtmlTags)}
                     />
                   </div>
 
@@ -170,7 +238,53 @@ export const Contact = () => {
                       // onClick={onSubmit}
                       type="submit"
                       disabled={submitting || pristine}>
-                      Submit
+                      {mutation.isPending ? (
+                        <svg
+                          className="spinner"
+                          width="24"
+                          height="24"
+                          fill={theme.accent}
+                          viewBox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg">
+                          <style></style>
+                          <g className="spinner">
+                            <circle cx="12" cy="2.5" r="1.5" opacity=".14" />
+                            <circle
+                              cx="16.75"
+                              cy="3.77"
+                              r="1.5"
+                              opacity=".29"
+                            />
+                            <circle
+                              cx="20.23"
+                              cy="7.25"
+                              r="1.5"
+                              opacity=".43"
+                            />
+                            <circle
+                              cx="21.50"
+                              cy="12.00"
+                              r="1.5"
+                              opacity=".57"
+                            />
+                            <circle
+                              cx="20.23"
+                              cy="16.75"
+                              r="1.5"
+                              opacity=".71"
+                            />
+                            <circle
+                              cx="16.75"
+                              cy="20.23"
+                              r="1.5"
+                              opacity=".86"
+                            />
+                            <circle cx="12" cy="21.5" r="1.5" />
+                          </g>
+                        </svg>
+                      ) : (
+                        "Send"
+                      )}
                     </button>
                     <button
                       type="button"
